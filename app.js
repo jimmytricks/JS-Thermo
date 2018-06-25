e11340d1e2abbbb814a7a18a286cc57bca71ea2a
James <jhixie4@gmail.com>
2018-06-25 16:43:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+(function raiseThermo (){
+
+    // get the thermo ID
+var element = document.getElementById("thermo");
+
+    // raise it by the amount
+    element.classList.add("raiseThermo");
+
+})();
