467a68a5a10cf55b8119ee02e8cce6f0fccca416
James <jhixie4@gmail.com>
2018-06-25 17:38:33 -0700

    added total num/sum of donations

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 (function raiseThermo() {
 
+    
+    // work out total of donations
+    var totalNumOfDonations = document.getElementsByClassName("donation");
+        var i;
+        // loop through amount of donations, tally to total sum
+        for (i = 0; i < totalNumOfDonations.length; i++){
+            var totalSumOfDonations = 0;
+            totalSumOfDonations =+ totalNumOfDonations[i].dataset.value;;
+            console.log(totalSumOfDonations);
+        }
 
     // set variable ready for storing pixel height of combined total
     var calculatedPixelAmount;
 
     // get the data value from donation goal
     var donationGoal = document.getElementById("donationgoal");
-    var donationGoalData = donationGoal.dataset.value;
+    var donationGoalData = donationGoal.dataset.valuemax;
     console.log(donationGoalData);
 
     // get the data value from combined total
@@ -16,13 +26,10 @@
 
     // work out how much the therm should rise
     function calcPercentage(donationGoalData, highestCurrentValueData) {
-
         // calc the pixel amount then times it by 500px (size of thermo)
         calculatedPixelAmount = (highestCurrentValueData / donationGoalData) * 500;
-
         // round to integer
-        var roundedCalculatedPixelAmount = Math.round(calculatedPixelAmount); 
-        
+        var roundedCalculatedPixelAmount = Math.round(calculatedPixelAmount);         
         // make string, add px
         var stringPixelAmount = roundedCalculatedPixelAmount.toString();
         stringPixelAmount += 'px';
